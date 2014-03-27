ca92decc988f6a18aba2bb9b8838719469c548c5
yezide <ob005696@etu.unice>
2014-03-27 16:39:41 +0100

    Ajout d'un comtroleur de pages statiques

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewApp::Application.config.secret_token = '8d7358305e1a29e1374f4054eccc6f0d08985f74e90b7db622898739ef4e3ef9b83eb4d3403c40da59be96b67bcea7ef12ea9c823d0f9f702ca1abb706c6436f'
